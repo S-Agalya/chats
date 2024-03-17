1092b8b2ba34a968013e0123de1ec96a7060c72f
S.Agalya <agalyaishere@gmail.com>
2024-03-17 12:12:31 +0530

    first commit

--- index.js
+++ index.js
@@ -1,49 +1,33 @@
-// import { readFileSync } from 'fs';
-
-// function chatsplitting() {
-// var all_chats=[] //stores all the splitted chats
-// var chat = readFileSync("./sample.txt", "utf-8").split(/\r?\n/);
-// for(var i=0;i<chat.length;i++){
-//     var splitted_chat=chat[i].split(/ - |: /)
-//     all_chats.push(splitted_chat)
-//     // console.log(splitted_chat)
-    
-// }return all_chats
-// }
-// function getting_ts_ph_text(splitted_chat1){
-//     var result=[]
-//     for(var i=0;i<splitted_chat1.length;i++){
-//         var inner_arr=splitted_chat1[i]  //contains the array inside the array(str1) .i.e [[str1][str2]]
-//         var ts=inner_arr[0]  //for timestamp
-//         var contact=inner_arr[1]  
-//         // console.log({ts,contact})
-//         result.push({ts,contact})
-//     }return result 
-
- 
-// }
-
-// var inputsplit=chatsplitting()
-// var splitted_chat1=getting_ts_ph_text(inputsplit)
-// console.log(splitted_chat1)
-
 
 
 import { readFileSync } from 'fs';
-function whatsappchat() {
+
 var chat = readFileSync("./sample.txt", "utf-8").split(/\r?\n/);
-for(var i=0;i<chat.length;i++){
-    var splitted_chat=chat[i].split(/ - |: /)   //splitting based on - and :
-    var ts=splitted_chat[0]
-    var contact=splitted_chat[1]
-    
-    if(contact && contact.startsWith('-') && contact.endsWith(':') && contact.length==14 ){
-        return contact
+var json_arr = [];
+
+for (var i = 0; i < chat.length; i++) {
+    var splitted_chat = chat[i].split(/ - |: /);
+    var ts = splitted_chat[0];
+    if (splitted_chat.length >= 2) {
+        var contact = splitted_chat[1];
+        var contactmatch = contact.match(/^[\d\s()+-]+$/) || contact.match(/^\w+$/);
         
+        if (contactmatch) {
+            var messages = splitted_chat.slice(2).join(": ");
+            json_arr.push({
+                timestamp: ts,
+                contact: contact,
+                messages: messages
+            });
+        } else {
+            var messages = splitted_chat.slice(1).join(": ");
+            json_arr.push({
+                timestamp: ts,
+                contact: null,
+                messages: messages
+            });
+        }
     }
-   
-     console.log({ts,contact})
+}
 
-    
-}}whatsappchat()
-
+console.log(json_arr);
