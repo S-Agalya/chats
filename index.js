b3bcc8a52718a1f38e178fc06a5a240e0200cd56
S.Agalya <agalyaishere@gmail.com>
2024-03-17 16:45:38 +0530

    first commit

--- index.js
+++ index.js
@@ -1,19 +1,64 @@
 
 //sample
-import { readFileSync } from 'fs';
+// import { readFileSync } from 'fs';
+// function whatsappchats(){
 
-var chat = readFileSync("./sample.txt", "utf-8").split(/\r?\n/);
-var json_arr = [];
 
-for (var i = 0; i < chat.length; i++) {
-    var splitted_chat = chat[i].split(/ - |: /);
-    var ts = splitted_chat[0];
-    if (splitted_chat.length >= 2) {
-        var contact = splitted_chat[1];
+// var chat = readFileSync("./sample.txt", "utf-8").split(/\r?\n/);
+// var json_arr = [];
+
+// for (var i = 0; i < chat.length; i++) {
+//     var splitted_chat = chat[i].split(/ - |: /);
+//     var ts = splitted_chat[0];
+//     if (splitted_chat.length >= 2) {
+//         var contact = splitted_chat[1];
+//         var contactmatch = contact.match(/^[\d\s()+-]+$/) || contact.match(/^\w+$/);
+        
+//         if (contactmatch) {
+//             var messages = splitted_chat.slice(2).join(": ");
+//             json_arr.push({
+//                 timestamp: ts,
+//                 contact: contact,
+//                 messages: messages
+//             });
+      
+//          }  
+//         else {
+//             var messages = splitted_chat.slice(1).join(": ");
+//             json_arr.push({
+//                 timestamp: ts,
+//                 contact: null,
+//                 messages: messages
+//             });
+//         }  
+
+//     }
+// }
+
+// console.log(json_arr);
+
+// }whatsappchats()
+
+
+
+//sample
+import {readFileSync} from 'fs'
+function readchatdata(){
+  var chat=readFileSync('sample.txt','utf-8').split(/\r?\n/)
+  return chat
+}
+function split_the_chats(){
+  var json_arr = [];
+  for(var i=0;i<all_chats.length;i++){
+    var splits=all_chats[i].split(/ - |: /)
+     var ts=splits[0]
+   
+    if (splits.length >= 2) {
+        var contact = splits[1];
         var contactmatch = contact.match(/^[\d\s()+-]+$/) || contact.match(/^\w+$/);
         
         if (contactmatch) {
-            var messages = splitted_chat.slice(2).join(": ");
+            var messages = splits.slice(2).join(": ");
             json_arr.push({
                 timestamp: ts,
                 contact: contact,
@@ -22,7 +67,7 @@ for (var i = 0; i < chat.length; i++) {
       
          }  
         else {
-            var messages = splitted_chat.slice(1).join(": ");
+            var messages = splits.slice(1).join(": ");
             json_arr.push({
                 timestamp: ts,
                 contact: null,
@@ -33,12 +78,18 @@ for (var i = 0; i < chat.length; i++) {
     }
 }
 
-console.log(json_arr);
-
+// console.log(json_arr);
+   return json_arr
+  } 
+    var all_chats=readchatdata()
+    var finding_ts_contact_messages=split_the_chats(all_chats)
+    console.log(finding_ts_contact_messages)
 
 //main prgm
 
 // import { readFileSync,writeFileSync } from 'fs';
+//function whatsappchats(){
+
 
 // var chat = readFileSync("./whatsappchats.txt", "utf-8").split(/\r?\n/);
 // var json_arr = [];
@@ -75,5 +126,6 @@ console.log(json_arr);
 //  writeFileSync('jsonfile', jsonFile);
 //  console.log("success");
 
+//}whatsappchats()
 
 
